bcc7a71a0e16153e6ac0eaae93cddea1d0eaa986
Heet1996 <heet1885@gmail.com>
2020-04-15 22:53:40 +0530

    Added skills page

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,37 @@
 import React from 'react';
-
-let Skills=(props)=>(<div>You're at Skill page</div>)
+import './Skills.scss';
+import Pdf from '../../Heet_Shah_New_Resume.pdf';
+let Skills=(props)=>(
+            <div className="skills">
+                <h3 className="skills-h3">Technology Skills</h3>
+                <div className="skills-area">
+                        <div className="skills-area__box">
+                            <h4>DAY-TO-DAY COMFORT</h4>
+                            <ul>
+                                <li>React JS</li>
+                                <li>React-Redux</li>
+                                <li>JavaScript ES6</li>
+                                <li>Node JS</li>
+                                <li>GitHub</li>
+                                <li>CSS3</li>
+                                <li>HTML</li>
+                            </ul>
+                        </div>
+                        <div className="border"></div>
+                        <div className="skills-area__box">
+                            <h4>EXPERIENCE WITH</h4>
+                            <ul>
+                                <li>Angular JS</li>
+                                <li>PHP</li>
+                                <li>Selenium</li>
+                                <li>Java</li>
+                                <li>Bootstrap</li>
+                                <li>Materliase CSS</li>
+                            </ul>
+                        </div>
+                </div>
+               <a href={Pdf} target="_blank"><button className="button button--ujarak button--border-thin button--text-thick button--inverted">Resume</button></a>
+            </div>
+)
 
 export default Skills;
