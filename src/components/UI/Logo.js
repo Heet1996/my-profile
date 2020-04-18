836d680f6f6803dfd9fa09322290c2da54ab76d3
Heet1996 <heet1885@gmail.com>
2020-04-18 23:35:51 +0530

    Final version added

--- src/components/UI/Logo.js
+++ src/components/UI/Logo.js
@@ -3,7 +3,7 @@ import './Logo.scss';
 
 let Logo=(props)=>(
     <div className="logo-space">
-        <img src={require('./Heet.png')} alt={"My Logo"} className="logo-space__img"/>
+        <a href="/"><img src={require('./Heet.png')} alt={"My Logo"} className="logo-space__img"/></a>
     </div>
 );
 
