ed937d4310db7a7828c34eaed4ef19a1a7fe9e63
Heet1885 <heet.shah@techno-soft.in>
2020-08-23 10:09:52 +0530

    Added reference

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ let About=(props)=>(
             <div className="about-section">
                 <div className="about-section__desc">
                 <p>Hi, I'm Heet Shah.</p>
-                <p>I'm a JavaScript Developer and formerly an Automation Test Engineer at Barclays and was a Full Stack Web Developer at Ascent Future Tech. 
+                <p>I'm a JavaScript Developer at <a href="https://makestories.io" target="_blank" rel="noopener noreferrer">MakeStories</a> and formerly an Automation Test Engineer at Barclays and was a Full Stack Web Developer at Ascent Future Tech. 
                 I've completed my Bachelors in Computer Science from VJTI, Mumbai in 2018 and my love to Web programming started during my college days.
                 </p>
                 <p>
--- src/components/Connect/Connect.js
+++ src/components/Connect/Connect.js
@@ -1,38 +1,60 @@
-import React from 'react';
+import React from "react";
 import "./Connect.scss";
-let Connect=(props)=>(
-<div className="connect">
-        
-                <div className="connect-area">
-                    <h3 className="connect-h3">CONTACT</h3>
-                    <h4 className="connect-h4">Feel to contact me through the email below, I'd love to chat.</h4>
-                    <a href="mailto:heet1885@gmail.com" class="email">heet1885@gmail.com</a>
-                </div>
-        
-                <ul className="connect-footer">
-                <li>
-                <a href="https://twitter.com/heet907" target="_blank" rel="noopener noreferrer" className="twitter"> 
-                    Twitter
-                </a>
-                </li>
-                <li>    
-                <a href="https://www.linkedin.com/in/heet1996" target="_blank" rel="noopener noreferrer" className="linkedin">
-                    LinkedIn
-                </a> 
-                </li>
-                <li>
-                <a href="https://www.facebook.com/heetshah1996" target="_blank" rel="noopener noreferrer" className="facebook">
-                    FaceBook
-                </a>
-                </li>
-                <li>    
-                <a href="https://github.com/Heet1996" target="_blank" rel="noopener noreferrer" className="github">
-                    GitHub
-                </a>
-                </li>   
-                </ul>
-        
+let Connect = (props) => (
+	<div className="connect">
+		<div className="connect-area">
+			<h3 className="connect-h3">CONTACT</h3>
+			<h4 className="connect-h4">
+				Feel to contact me through the email below, I'd love to chat.
+			</h4>
+			<a href="mailto:heet1885@gmail.com" class="email">
+				heet1885@gmail.com
+			</a>
+		</div>
 
-</div>);
+		<ul className="connect-footer">
+			<li>
+				<a
+					href="https://twitter.com/heet907"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="twitter"
+				>
+					Twitter
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://www.linkedin.com/in/heet1996"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="linkedin"
+				>
+					LinkedIn
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://www.facebook.com/heetshah1996"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="facebook"
+				>
+					FaceBook
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://github.com/Heet1996"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="github"
+				>
+					GitHub
+				</a>
+			</li>
+		</ul>
+	</div>
+);
 
 export default Connect;
